6bfb1568dcef0c5a6e01dccafb1c50c392c794a4
Stephen Willingham <gowillingham@gmail.com>
2012-12-15 09:37:34 -0600

    Add Payment.paid? and tests.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,20 @@
 class Payment < ActiveRecord::Base
 	belongs_to :registration
+
+	validates :registration_id,
+		:presence => true	
+
+  def paid?
+    if scholarship
+      true
+    else
+      if amount.nil?
+        false
+      elsif amount == 0
+        false
+      else
+        true
+      end
+    end
+  end
 end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -27,10 +27,63 @@ describe Payment do
   	end.should change(Payment, :count).by(1)
   end
 
-  context "methods" do
-  	it "includes .registration" do
-  		product = Payment.create! @attr
-  		product.should respond_to(:registration)
-  	end
+  context "validations" do
+
+    it "should require a registration" do
+      Payment.new(@attr.merge(:registration_id => nil)).should_not be_valid
+    end
   end
+
+  context "method" do
+
+    context ".paid?" do
+
+      it "exists" do
+        Payment.create(@attr).should respond_to(:paid?)
+      end
+
+      it "should be true if scholarship is true or the payment amount is greater than zero" do
+        payment = Payment.create(@attr.merge(:amount => 70))
+        payment.paid?.should be_true
+        
+        payment.amount = nil
+        payment.scholarship = true
+        payment.save
+        payment.paid?.should be_true
+        
+        payment.amount = 0
+        payment.scholarship = true
+        payment.save
+        payment.paid?.should be_true
+        
+        payment.scholarship = false
+        payment.save
+        payment.paid?.should_not be_true
+      end
+    end
+
+    context ".receive_paypal_payment" do
+
+      it "exists"
+
+      context "when acknowledged by paypal" do
+        it "should update the payment when payment_status is 'Complete'"
+        it "should not update the payment when mc_gross <> product.price"
+        it "should not update the amount for payment_status 'Pending'"
+        it "should not update the amount for payment_status 'Failed'"
+      end 
+
+      context "when not acknowledged by paypal" do
+        it "should not update the payment"
+      end
+    end
+
+  	context ".registration" do
+
+      it "exists" do 
+    		product = Payment.create! @attr
+    		product.should respond_to(:registration)
+      end
+  	end
+  end  
 end
