5706d895f65247988022babc6e30a301ca514c7c
Stephen Willingham <gowillingham@gmail.com>
2013-03-02 17:26:33 -0600

    Add paid? column to registrations.

--- spec/views/registrations/index.html.erb_spec.rb
+++ spec/views/registrations/index.html.erb_spec.rb
@@ -3,57 +3,23 @@ require 'spec_helper'
 describe "registrations/index" do
   before(:each) do
     @product = Factory(:product)
-    assign(:registrations, [
-      stub_model(Registration,
-        :first_name => 'first_name', 
-        :last_name => 'last_name', 
-        :parent_first_name => 'parent_first_name', 
-        :parent_last_name => 'parent_last_name', 
-        :email => 'email@example.com', 
-        :address => 'address', 
-        :phone => '999 999 9999', 
-        :city => 'city', 
-        :state => 'state', 
-        :tshirt_size => 'YL', 
-        :parent_tshirt_size => 'XL', 
-        :grade => '5', 
-        :school => 'school', 
-        :note => 'note', 
-        :waiver => true, 
-        :parent_helper => true, 
-        :volleyball => true, 
-        :product_id => @product.id
-      ),
-      stub_model(Registration,
-        :first_name => 'first_name', 
-        :last_name => 'last_name', 
-        :parent_first_name => 'parent_first_name', 
-        :parent_last_name => 'parent_last_name', 
-        :email => 'email@example.com', 
-        :address => 'address', 
-        :phone => '999 999 9999', 
-        :city => 'city', 
-        :state => 'state', 
-        :tshirt_size => 'YL', 
-        :parent_tshirt_size => 'XL', 
-        :grade => '5', 
-        :school => 'school', 
-        :note => 'note', 
-        :waiver => true, 
-        :parent_helper => true, 
-        :volleyball => true, 
-        :product_id => @product.id
-      )
-    ])
+    @registration_1 = Factory(:registration, :product => @product)
+    @registration_1.payments << Factory(:payment, :registration_id => @registration_1.id)
+    @registration_2 = Factory(:registration, :product => @product)
+    @registration_2.payments << Factory(:payment, :registration_id => @registration_2.id)
+    assign(:registrations, [@registration_1, @registration_2])
   end
 
   it "renders a list of registrations" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "last_name, first_name".to_s, :count => 2
-    assert_select "tr>td", :text => "email@example.com".to_s, :count => 2
-    assert_select "tr>td", :text => "5".to_s, :count => 2
-    assert_select "tr>td", :text => "school".to_s, :count => 2
+    assert_select "tr>td", :text => "#{@registration_1.last_name}, #{@registration_1.first_name}".to_s
+    assert_select "tr>td", :text => @registration_1.email.to_s
+    assert_select "tr>td", :text => @registration_2.email.to_s
+    assert_select "tr>td", :text => @registration_1.grade.to_s
+    assert_select "tr>td", :text => @registration_2.grade.to_s
+    assert_select "tr>td", :text => @registration_1.school.to_s
+    assert_select "tr>td", :text => @registration_2.school.to_s
     assert_select "tr>td", :text => @product.code.to_s, :count => 2
   end
 end
